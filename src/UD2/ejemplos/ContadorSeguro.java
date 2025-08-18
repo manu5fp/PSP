cf9f3da328d85839b0440f1646891d53c5a0524e
manu <manu@manu-Server>
2025-08-18 14:29:29 +0200

    act

---
+++ src/UD2/ejemplos/ContadorSeguro.java
@@ -0,0 +1,11 @@
+package UD2.ejemplos;
+
+class ContadorSeguro {
+    private int cuenta = 0;
+    public void incrementar() {
+        cuenta++;
+    }
+    public int getCuenta() {
+        return cuenta;
+    }
+}
